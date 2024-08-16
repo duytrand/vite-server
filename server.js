77cce3b3845ed6a3f2c28c628d53a8df5785e39f
Duy Tran <duytran_d@yahoo.com>
2024-08-16 18:42:18 -0500

    port 443

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get("/api", (req, res) => {
   res.json({ fruits: ["Du2y", "strawberry", "pineapple"] });
 });
 
-app.listen(8080, () => {
+app.listen(443, () => {
   console.log("Server started on port 8080");
 });
